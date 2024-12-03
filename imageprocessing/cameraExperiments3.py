f36e07cda42a493057984dd795742d26cc1caa83
ShakedDanZilberman <shaked.zilberman.45@gmail.com>
2024-12-03 21:01:42 +0200

    Something that kinda works. Fine for testing

--- imageprocessing/cameraExperiments3.py
+++ imageprocessing/cameraExperiments3.py
@@ -81,6 +81,10 @@ class FirstNImagesHandler:
             diff = differenceImage(img, self.getAverage())
             diff = blurImage(diff, 20)
             diff = aboveThreshold(diff, 30)
+            # convert image to RBG
+            diff = cv2.cvtColor(diff, cv2.COLOR_GRAY2BGR)
+            # find objects in the image
+            diff = find_objects(diff)
             cv2.imshow(TITLE, diff)
 
 def processImage(img):
@@ -97,7 +101,7 @@ def showEdges(img):
     black = np.zeros_like(img)
     cv2.drawContours(black, contours, -1, (255, 255, 255), 1)
     # Show
-    cv2.imshow('Contours', black)
+    # cv2.imshow('Contours', black)
     cv2.imshow('Edges', edges)
 
 def differenceImage(img1, img2):
@@ -155,6 +159,45 @@ def detectCameras():
     # Show all images in matplotlib window
     showMultipleFrames(imgs, [f'Camera {i}' for i in range(MAX_CAMERAS)], f'Failed to connect to camera #{CAMERA_INDEX}\nAll Available Cameras')
     plt.show()
+
+def find_objects(img):
+    # This is a very primitive scheme of finding and merging objects.
+    # It ignores the pixel-connectedness of close objects and just cares about the distance.
+    dx = 10
+    dy = 10
+    stepx = stepy = 5
+    threshold = 120
+    pixel_distance_merging = 15
+    objects = []
+    for x in range(0, img.shape[1] - dx, stepx):
+        for y in range(0, img.shape[0] - dy, stepy):
+            roi = img[y:y+dy, x:x+dx]
+            if np.mean(roi) > threshold:
+                objects.append((x, y))
+
+    # Merge close objects
+    distance_squared = lambda obj1, obj2: (obj1[0] - obj2[0])**2 + (obj1[1] - obj2[1])**2
+    # Merge close objects with complexity O(n^2)
+    merged_objects = []
+    for obj in objects:
+        merged = False
+        for i in range(len(merged_objects)):
+            if distance_squared(obj, merged_objects[i]) < pixel_distance_merging**2:
+                merged_objects[i] = ((obj[0] + merged_objects[i][0]) // 2, (obj[1] + merged_objects[i][1]) // 2)
+                merged = True
+                break
+        if not merged:
+            merged_objects.append(obj)
+
+    objects = merged_objects
+    
+    for x, y in objects:
+        cv2.rectangle(img, (x, y), (x+dx, y+dy), (0, 255, 0), 1)
+        # point with coordinates at the center of the rectangle
+        cv2.circle(img, (x+dx//2, y+dy//2), 3, (0, 255, 0), -1)
+        # label the point
+        cv2.putText(img, f'({x+dx//2}, {y+dy//2})', (x+dx//2, y+dy//2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+    return img
 
 
 def main():
