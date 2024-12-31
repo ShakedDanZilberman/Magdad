f7487a93760ddc75f6035f917eb1b4a859c013f9
yuvyuvyuv <62467132+yuvyuvyuv@users.noreply.github.com>
2024-12-31 21:01:27 +0200

    refactoring names and default values

--- system/pid.py
+++ system/pid.py
@@ -1,24 +1,25 @@
 import time
 import numpy as np
 
-# PID constants
-Kp = 0.1
-Ki = 0.1
-Kd = 0
 
 class PID:
-    def __init__(self):
+    def __init__(self, Kp=0.1, Ki=0.1, Kd=0):
         """
         Initialize PID controller
 
         Initializes the PID controller with the previous time, integral and error values set to 0
         """
+        # PID constants
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+
         # Initialize previous values for PID
         self.time_prev = time.time() / 100
         self.integral = np.array([0, 0])
         self.error_prev = np.array([0, 0])
 
-    def set_constants(new_Kp, new_Ki, new_Kd):
+    def set_constants(self, new_Kp, new_Ki, new_Kd):
         """
         Set new PID constants
 
@@ -27,36 +28,30 @@ class PID:
             new_Ki (float): new Integral constant
             new_Kd (float): new Derivative constant
         """
-        global Kp, Ki, Kd
-        Kp = new_Kp
-        Ki = new_Ki
-        Kd = new_Kd
+        self.Kp = new_Kp
+        self.Ki = new_Ki
+        self.Kd = new_Kd
         return
 
-    def PID(target: tuple[float, float], curr: tuple[float, float], Kp=Kp, Ki=Ki, Kd=Kd):
+    def PID(self, target: tuple[float, float], curr: tuple[float, float]):
         """
         PID controller for the system, returns the angle to turn to reach the target position
 
         Args:
             target (tuple[float, float]): target position to reach
             curr (tuple[float, float]): current position of the system
-            Kp (float, optional): Proportional constant. Defaults to Kp.
-            Ki (float, optional): Integral constant. Defaults to Ki.
-            Kd (float, optional): Derivative constant. Defaults to Kd.
 
         Returns:
             float: angle to turn to reach the target position
         """
         # target and curr are (x, y)
-        global integral, time_prev, error_prev
-
         now = time.time() / 100
         error = np.array([target - curr])
 
-        P = Kp * error
-        I = integral = integral + Ki * error * (now - time_prev)
-        D = Kd * (error - error_prev) / (now - time_prev)
+        P = self.Kp * error
+        I = self.integral + self.Ki * error * (now - self.time_prev)
+        D = self.Kd * (error - self.error_prev) / (now - self.time_prev)
 
-        error_prev = error
-        time_prev = now
+        self.error_prev = error
+        self.time_prev = now
         return P + I + D
