3112f66a4e09fc66f7dddbc45cc113d86e9c5556
ayala plesner <154817039+ayaluluples@users.noreply.github.com>
2025-01-12 23:42:42 +0200

    עדכוניםםםם

--- yolo/vol1.py
+++ yolo/vol1.py
@@ -1,10 +1,17 @@
-from ultralytics import YOLO
+from roboflow import Roboflow
 
-# Load a pretrained YOLO model
-model = YOLO("yolov8.pt", "yolov11n.pt", device="cpu")
-# Perform object detection on an image
-results = model("yolo\images\Screenshot 2025-01-09 204222.jpg")
-# Visualize the results
-results[0].show()
-# Export the model to ONNX format
-#success = model.export(format="onnx")
+
+rf = Roboflow(api_key='tYdBc3fC65ADwcIsYT2D')
+project = rf.workspace('ayalulu').project('camera-training-ltw8l')
+# Download the dataset
+dataset = project.version(1).download('yolov8')
+print("finished downloading dataset")
+# from ultralytics import YOLO
+
+# #Load a pretrained YOLO model
+# model = YOLO("yolo\yolov8m-seg.pt")
+# # Perform object detection on an image
+# results = model("yolo\images\Screenshot 2025-01-09 204149.jpg")
+# # Visualize the results
+# results[0].show()
+
