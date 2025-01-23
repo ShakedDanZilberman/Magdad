9db487b7dec44a45a172128d6f7e5ecb9f17737e
ayala plesner <154817039+ayaluluples@users.noreply.github.com>
2025-01-23 14:30:10 +0200

    Create lidar_new.ino

---
+++ system/lidar_new.ino
@@ -0,0 +1,39 @@
+// Example Arduino program for reading the Pololu Distance Sensor with Pulse Width Output, 300cm Max
+ 
+// Change this to match the Arduino pin connected to the sensor's OUT pin.
+const uint8_t sensorPin = A5;
+ 
+void setup()
+{
+  Serial.begin(115200);
+}
+ 
+void loop()
+{
+  int16_t t = pulseIn(sensorPin, HIGH);
+
+ 
+  if (t == 0)
+  {
+    // pulseIn() did not detect the start of a pulse within 1 second.
+    Serial.println("timeout");
+  }
+  else if (t > 1850)
+  {
+    // No detection.
+    Serial.println(-1);
+  }
+  else
+  {
+    // Valid pulse width reading. Convert pulse width in microseconds to distance in millimeters.
+    int16_t d = (t - 1000)*4;
+ 
+    // Limit minimum distance to 0.
+    if (d < 0) { d = 0; } 
+
+    Serial.print(d);
+    Serial.println(" mm");
+  
+
+  }
+}
