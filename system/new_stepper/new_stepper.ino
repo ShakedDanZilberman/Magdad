a9e4135a3ebc139a5d29d0ca54c0968b5ef22e4c
yuvyuvyuv <62467132+yuvyuvyuv@users.noreply.github.com>
2025-05-14 19:29:30 +0300

    מלא כיף

--- system/new_stepper/new_stepper.ino
+++ system/new_stepper/new_stepper.ino
@@ -1,62 +1,32 @@
 #include <AccelStepper.h>
 
 // Pins
-const int gunPin_I = 10;
-const int gunPin_II = 9;
-const int dirPin_A = 4;
-const int stepPin_A = 7;
-const int dirPin_B = 2;
-const int stepPin_B = 5;
-const int enablePin = 8;
+const int gunPin = 4;
+const int dirPin = 2;
+const int stepPin = 3;
+const int enablePin = 5;
 const int SHOOT_COOLDOWN = 2;  // ms
 const int MAX_SPEED = 3000;  // steps per second
 const int ACCELERATION = 1000;  // steps per second^2
 
 // Stepper setup: DRIVER interface type = 1
-AccelStepper stepper_A(AccelStepper::DRIVER, stepPin_A, dirPin_A);
-AccelStepper stepper_B(AccelStepper::DRIVER, stepPin_B, dirPin_B);
+AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);
+
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(gunPin_I, OUTPUT);
-  pinMode(gunPin_II, OUTPUT);
+  pinMode(gunPin, OUTPUT);
   pinMode(enablePin, OUTPUT);
-  digitalWrite(enablePin, HIGH);  // Disable driver
 
+  stepper.setEnablePin(enablePin);
+
+  stepper.disableOutputs();
 
   // Stepper setup
-  stepper_A.setMaxSpeed(MAX_SPEED);        
-  stepper_A.setAcceleration(ACCELERATION);     
-  stepper_A.setCurrentPosition(0);    // reset position
-  stepper_B.setMaxSpeed(MAX_SPEED);
-  stepper_B.setAcceleration(ACCELERATION);
-  stepper_B.setCurrentPosition(0);    // reset position
-}
-
-void shoot(int gunPin) {
-  digitalWrite(gunPin, HIGH);
-  Serial.println("Shooting.");
-  delay(SHOOT_COOLDOWN);
-  digitalWrite(gunPin, LOW);
-  Serial.println("Done"); // Critical for communication with Python
-}
-
-void rotate(AccelStepper stepper, int enablePin, long steps) {
-  Serial.print("Requested steps: ");
-  Serial.println(steps);
-
-  digitalWrite(enablePin, LOW);  // Enable driver
-  long target = stepper.currentPosition() + steps;
-  stepper.moveTo(target);
-
-  // Run motor until it reaches the target
-  while (stepper.distanceToGo() != 0) {
-    stepper.run();
-  }
-
-  digitalWrite(enablePin, HIGH);  // Disable driver after move
-  Serial.println("Done"); // Critical for communication with Python
+  stepper.setMaxSpeed(MAX_SPEED);        
+  stepper.setAcceleration(ACCELERATION);     
+  stepper.setCurrentPosition(0);    // reset position
 }
 
 
@@ -65,16 +35,24 @@ void loop() {
     String command = Serial.readStringUntil('\n');
     command.trim();
 
-    if (command == "SHOOT1") {
-      shoot(gunPin_I);
-    } else if (command == "SHOOT2") {
-      shoot(gunPin_II);
-    } else if (command.startsWith("ROTATEA:")) {
+    if (command == "SHOOT") {
+      digitalWrite(gunPin, HIGH);
+      Serial.println("Pew Pew");
+      delay(SHOOT_COOLDOWN);
+      digitalWrite(gunPin, LOW);
+      Serial.println("Done"); // Critical for communication with Python
+    } 
+    else if (command.startsWith("ROTATE:")) {
       long steps = command.substring(8).toInt();
-      rotate(stepper_A, enablePin, steps);
-    } else if (command.startsWith("ROTATEB:")) {
-      long steps = command.substring(8).toInt();
-      rotate(stepper_B, enablePin, steps);
+      Serial.print("Requested steps: ");
+      Serial.println(steps);
+      stepper.move(steps);
+    }
+
+    if (stepper.distanceToGo() != 0){
+      stepper.enableOutputs();
+      stepper.run();
+      stepper.disableOutputs();
     }
   }
 }
