1acf52378894d2704bc42a54bbaa3e5857ac9531
yuvyuvyuv <62467132+yuvyuvyuv@users.noreply.github.com>
2025-05-15 14:17:23 +0300

    שינויים של יובי

--- system/new_stepper/new_stepper.ino
+++ system/new_stepper/new_stepper.ino
@@ -1,13 +1,29 @@
 #include <AccelStepper.h>
 
-// Pins
-const int gunPin = 2;
+/*
+CNC pins Z - 
+const int gunPin = 12;
+const int dirPin = 4;
+const int stepPin = 7;
+const int enablePin = 8;
+
+
+
+bodoboard - 
+const int gunPin = 4;
+const int dirPin = 3;
+const int stepPin = 2;
+const int enablePin = 5;
+*/
+
+const int gunPin = 12;
 const int dirPin = 4;
 const int stepPin = 7;
 const int enablePin = 8;
 const int SHOOT_COOLDOWN = 2;  // ms
-const int MAX_SPEED = 2000;  // steps per second
+const int MAX_SPEED = 3000;  // steps per second
 const int ACCELERATION = 10000;  // steps per second^2
+long count = 0;
 
 // Stepper setup: DRIVER interface type = 1
 AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);
@@ -15,13 +31,13 @@ AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);
 
 void setup() {
   Serial.begin(9600);
-
   pinMode(gunPin, OUTPUT);
   pinMode(enablePin, OUTPUT);
 
-  // stepper.setEnablePin(enablePin);
-
-  // stepper.disableOutputs();
+  stepper.setEnablePin(enablePin);
+  // stepper.enableOutputs();
+  stepper.setPinsInverted(false, false, true);
+  stepper.disableOutputs();
 
   // Stepper setup
   stepper.setMaxSpeed(MAX_SPEED);        
@@ -31,19 +47,6 @@ void setup() {
 
 
 void loop() {
-  // if (stepper.distanceToGo() == 0){
-  //   stepper.disableOutputs();
-  // }else{
-  //   stepper.enableOutputs();
-  // }
-  stepper.run();
-    // if (stepper.run()){
-    //   stepper.enableOutputs();
-    // } 
-    // else{
-    //   stepper.disableOutputs();
-    // }
-
   if (Serial.available()) {
     String command = Serial.readStringUntil('\n');
     command.trim();
@@ -56,10 +59,22 @@ void loop() {
       Serial.println("Done"); // Critical for communication with Python
     } 
     else if (command.startsWith("ROTATE:")) {
+      stepper.enableOutputs();
       long steps = command.substring(7).toInt();
       Serial.print("Requested steps: ");
       Serial.println(steps);
       stepper.move(steps);
+      Serial.println("Done"); // Critical for communication with Python
+    }else if(command =="OFF"){
+      stepper.disableOutputs();
+      Serial.println("Turned off.");
+    }else if(command =="ON"){
+      stepper.enableOutputs();
+      Serial.println("Turned on.");
     }
   }
+  if (stepper.currentPosition() == stepper.targetPosition()){
+    stepper.disableOutputs();
+  }
+  stepper.run();
 }
